d5c36e7cbb5aaaf66ae05dd81f374e7d0d4f12ec
SomilGumber <somilgumber05@gmail.com>
2023-12-13 01:14:08 +0530

    added files

---
+++ index.js
@@ -0,0 +1,14 @@
+let count = 0;
+let countEl = document.getElementById('counter');
+let saveEl = document.getElementById('save-el')
+function increment() {
+  count += 1;
+  countEl.textContent = count;
+}
+
+function save() {
+  let countText = count + " - ";
+  saveEl.textContent += countText;
+  countEl.textContent = 0;
+  count = 0;
+}
